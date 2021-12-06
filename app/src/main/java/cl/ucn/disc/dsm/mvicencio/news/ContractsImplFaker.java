10f8e92b127fd1fe7bed0cd81fa7fc12e3088385
Miguel Vicencio <miguel.vicenciog@gmail.com>
2021-12-06 07:56:10 -0300

    chore(Project): add ContractsImplFaker and edit contracts class

--- app/src/main/java/cl/ucn/disc/dsm/mvicencio/news/ContractsImplFaker.java
+++ app/src/main/java/cl/ucn/disc/dsm/mvicencio/news/ContractsImplFaker.java
@@ -21,18 +21,43 @@
 
 package cl.ucn.disc.dsm.mvicencio.news;
 
+import com.github.javafaker.Faker;
+import java.util.ArrayList;
 import java.util.List;
+import org.threeten.bp.ZoneId;
+import org.threeten.bp.ZonedDateTime;
 
 /**
- * the Contracts of the news Project
  * @author Miguel Vicencio Gomez
+ * Contracts Implementation of News With Faker
  */
-public interface Contracts {
+public class ContractsImplFaker implements Contracts{
 
   /**
-   *
    * @return all the News in the backend ordered by published
    */
-  List<News> retrieveNews(int size);
+  @Override
+  public List<News> retrieveNews(int size) {
+    //faker provider
+    Faker faker = new Faker();
+    //the list to return
+    final List<News> newsList = new ArrayList<>();
 
+    for(int i = 0 ; i< size; i++){
+      News news = new News(
+          faker.superhero().name(),
+          faker.artist().name(),
+          faker.name().firstName(),
+          faker.internet().url(),
+          faker.internet().url(),
+          faker.backToTheFuture().quote(),
+          faker.starTrek().villain(),
+          ZonedDateTime.now(ZoneId.of("-4"))
+      );
+
+      newsList.add(news);
+
+    }
+    return newsList;
+  }
 }
